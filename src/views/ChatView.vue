a822e4768e2d6bee4d94b1704676a5928394ce37
TrendingMatteo <matteo.rosoli@trendingtopics.at>
2024-02-12 11:44:32 +0100

    add toggle checkbox

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,89 +1,80 @@
 <script setup lang="ts">
-import { Input } from "@/components/ui/input";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Navigation } from "lucide-vue-next";
-import MessageComponent from "@/components/customUi/MessageComponent.vue";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { ref, watch } from "vue";
-import SenderEnum from "@/model/SenderEnum";
-import MessageClass from "@/model/MessageClass";
-import { sendMsg, receivedMsg, startConversation } from "@/http/websocket";
-import NavigationBarComponent from "@/components/customUi/NavigationBarComponent.vue";
-import type { AIResponseInterface } from "@/model/AIResponseInterface";
-import Logo from "../assets/images/logo.svg";
-import LogoWhite from "../assets/images/logoWhite.svg";
-import Checkbox from "@/components/ui/checkbox/Checkbox.vue";
-import { useDarkModeStore } from "@/stores/DarkMode";
+import { Input } from '@/components/ui/input'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Navigation } from 'lucide-vue-next'
+import MessageComponent from '@/components/customUi/MessageComponent.vue'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { ref, watch } from 'vue'
+import SenderEnum from '@/model/SenderEnum'
+import MessageClass from '@/model/MessageClass'
+import { sendMsg, receivedMsg, startConversation } from '@/http/websocket'
+import NavigationBarComponent from '@/components/customUi/NavigationBarComponent.vue'
+import type { AIResponseInterface } from '@/model/AIResponseInterface'
+import Logo from '../assets/images/logo.svg'
+import LogoWhite from '../assets/images/logoWhite.svg'
+import Checkbox from '@/components/ui/checkbox/Checkbox.vue'
+import { useDarkModeStore } from '@/stores/DarkMode'
 
-const chatArray = ref<MessageClass[]>([]);
-const DarkModeStore = useDarkModeStore();
-const currentInput = ref("");
-const finished = ref(true);
-const session_key = ref<string>("");
+const chatArray = ref<MessageClass[]>([])
+const DarkModeStore = useDarkModeStore()
+const currentInput = ref('')
+const finished = ref(true)
+const session_key = ref<string>('')
+const formClicked = ref(false)
 
 watch(currentInput, () => {
   if (finished.value === true) {
-    if (currentInput.value === "") {
-      finished.value = false;
+    if (currentInput.value === '') {
+      finished.value = false
     } else {
-      finished.value = true;
+      finished.value = true
     }
   }
-});
+})
 
 function startChat() {
   startConversation((msg: any) => {
-    if (msg["SESSION-KEY"]) {
-      session_key.value = msg["SESSION-KEY"];
-      console.log(session_key.value);
+    if (msg['SESSION-KEY']) {
+      session_key.value = msg['SESSION-KEY']
+      console.log(session_key.value)
     }
-    const newTimestamp = new Date();
+    const newTimestamp = new Date()
 
-    handleReceive(msg, newTimestamp);
-  });
+    handleReceive(msg, newTimestamp)
+  })
 }
 
 function handleReceive(msg: AIResponseInterface, timestamp: Date) {
-  if (msg.value === "END" && msg.type === "STATUS") {
-    chatArray.value[chatArray.value.length - 1].state = true;
-    finished.value = true;
-    return;
+  if (msg.value === 'END' && msg.type === 'STATUS') {
+    chatArray.value[chatArray.value.length - 1].state = true
+    finished.value = true
+    return
   }
-  if (msg.value === "BEGIN" && msg.type === "STATUS") {
-    chatArray.value.push(new MessageClass(timestamp, "", SenderEnum.AI, false));
-    finished.value = false;
-    return;
+  if (msg.value === 'BEGIN' && msg.type === 'STATUS') {
+    chatArray.value.push(new MessageClass(timestamp, '', SenderEnum.AI, false))
+    finished.value = false
+    return
   }
-  const lastMessage = chatArray.value[chatArray.value.length - 1];
-  if (lastMessage && msg.value !== "START" && msg.type !== "STATUS") {
-    lastMessage.content += msg.value;
+  const lastMessage = chatArray.value[chatArray.value.length - 1]
+  if (lastMessage && msg.value !== 'START' && msg.type !== 'STATUS') {
+    lastMessage.content += msg.value
   }
 }
 
-function handleSend(
-  content: string,
-  timestamp: Date,
-  sender: SenderEnum,
-  session_key: string
-) {
-  if (session_key === "") {
-    return null;
+function handleSend(content: string, timestamp: Date, sender: SenderEnum, session_key: string) {
+  if (session_key === '') {
+    return null
   }
-  const newMessage = new MessageClass(timestamp, content, sender, true);
-  chatArray.value.push(newMessage);
-  console.log(session_key);
+  const newMessage = new MessageClass(timestamp, content, sender, true)
+  chatArray.value.push(newMessage)
+  console.log(session_key)
 
-  sendMsg(newMessage.content, session_key);
+  sendMsg(newMessage.content, session_key)
   receivedMsg((msg: AIResponseInterface) => {
-    handleReceive(msg, timestamp);
-  });
-  currentInput.value = "";
+    handleReceive(msg, timestamp)
+  })
+  currentInput.value = ''
 }
 </script>
 
@@ -116,35 +107,29 @@ function handleSend(
           <div v-else>
             <img :src="LogoWhite" alt="Logo" class="w-72 h-20" />
           </div>
-          <h1
-            class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
-          >
+          <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             Startup-Interviewer 🚀
           </h1>
           <div class="pt-5">
             <p class="text-xl text-muted-foreground">
-              Starte hier dein AI-Interview mit Trending Topics. Voll
-              automatisiert!
+              Starte hier dein AI-Interview mit Trending Topics. Voll automatisiert!
             </p>
           </div>
           <!-- <div class="text-center text-lg font-semibold px-36 pt-10">Some Werbung</div> -->
           <div class="flex items-center space-x-2 pt-20 pb-5">
-            <Checkbox id="terms" />
+            <input type="checkbox" id="terms" v-model="formClicked" />
             <label
               for="terms"
               class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               Ich habe die
               <u
-                ><a
-                  href="https://www.trendingtopics.eu/datenschutz/"
-                  target="_blank"
+                ><a href="https://www.trendingtopics.eu/datenschutz/" target="_blank"
                   >Datenschutzerklärung</a
                 ></u
               >
-              gelesen und verstanden, dass der Inhalt dieses Interviews von der
-              ausgewählten AI (+ dessen Anbieter) und Trending Topics
-              verarbeitet wird.
+              gelesen und verstanden, dass der Inhalt dieses Interviews von der ausgewählten AI (+
+              dessen Anbieter) und Trending Topics verarbeitet wird.
             </label>
           </div>
           <div class="">
@@ -155,17 +140,12 @@ function handleSend(
       <div class="flex justify-center">
         <Card class="w-[70rem]">
           <CardHeader> </CardHeader>
-          <CardContent>
+          <CardContent v-if="formClicked">
             <div class="flex justify-center items-center gap-7">
               <Input
                 @keyup.enter="
                   finished
-                    ? handleSend(
-                        currentInput,
-                        new Date(),
-                        SenderEnum.USER,
-                        session_key
-                      )
+                    ? handleSend(currentInput, new Date(), SenderEnum.USER, session_key)
                     : null
                 "
                 class="w-[50rem] p-6 text-base"
@@ -173,14 +153,7 @@ function handleSend(
                 placeholder="Schreibe deine Nachricht..."
               />
               <Button
-                @click="
-                  handleSend(
-                    currentInput,
-                    new Date(),
-                    SenderEnum.USER,
-                    session_key
-                  )
-                "
+                @click="handleSend(currentInput, new Date(), SenderEnum.USER, session_key)"
                 :disabled="!finished"
               >
                 <div class="flex items-center gap-2">
@@ -192,27 +165,17 @@ function handleSend(
           </CardContent>
           <CardFooter>
             <div class="flex justify-center w-full">
-              <div
-                class="text-sm text-muted-foreground flex justify-between w-[50%]"
-              >
+              <div class="text-sm text-muted-foreground flex justify-between w-[50%]">
                 <p>
-                  <a
-                    href="https://www.trendingtopics.eu/imprint/"
-                    target="_blank"
-                    >Impressum</a
-                  >
+                  <a href="https://www.trendingtopics.eu/imprint/" target="_blank">Impressum</a>
                 </p>
                 <p>
-                  <a
-                    href="https://www.trendingtopics.eu/datenschutz/"
-                    target="_blank"
+                  <a href="https://www.trendingtopics.eu/datenschutz/" target="_blank"
                     >Datenschutzerklärung</a
                   >
                 </p>
                 <p>
-                  <a href="https://newsrooms.ai/" target="_blank"
-                    >newsrooms.ai</a
-                  >
+                  <a href="https://newsrooms.ai/" target="_blank">newsrooms.ai</a>
                 </p>
               </div>
             </div>
